8a8cb421d4350c68a058dcfdb499ba3ab571ee95
lauraaac <lceballosa@unal.edu.co>
2022-04-09 23:22:39 -0500

    add model Usuario

--- doggieHommie/models.py
+++ doggieHommie/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+#Se crea entidad usuario
+class Usuario(models.Model):
+    TIPO_DOCUMENTOS = (
+        ('CC', 'Cédula de ciudadanía'),
+        ('TI', 'Tarjeta de identidad'),
+        ('CE', 'Cédula de extrajería'),
+    )
+    primer_nombre = models.CharField(max_length=30)
+    segundo_nombre = models.CharField(max_length=30)
+    primer_apellido = models.CharField(max_length=30)
+    segundo_apellido = models.CharField(max_length=30)
+    correo_electronico = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=10)
+    tipo_documento = models.CharField(max_length=30, choices= TIPO_DOCUMENTOS)
+    numero_documento = models.CharField(max_length=10)
+    pais = models.CharField(max_length=40)
+    departamento = models.CharField(max_length=40)
+    ciudad = models.CharField(max_length=40)
+    contrasenia = models.CharField(max_length=40)
+    estado = models.CharField(max_length=40)
+
+
