d8c54257b2116392db3e7c760be5ea84ea6ef79a
lceballosa1 <>
2022-04-11 00:32:39 -0500

    add models

--- doggieHommie/models.py
+++ doggieHommie/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 #Se crea entidad usuario
-class Usuario(models.Model):
+class User(models.Model):
     TIPO_DOCUMENTOS = (
         ('CC', 'Cédula de ciudadanía'),
         ('TI', 'Tarjeta de identidad'),
@@ -11,7 +11,7 @@ class Usuario(models.Model):
     segundo_nombre = models.CharField(max_length=30)
     primer_apellido = models.CharField(max_length=30)
     segundo_apellido = models.CharField(max_length=30)
-    correo_electronico = models.CharField(max_length=50)
+    correo_electronico = models.EmailField(max_length=50)
     telefono = models.CharField(max_length=10)
     tipo_documento = models.CharField(max_length=30, choices= TIPO_DOCUMENTOS)
     numero_documento = models.CharField(max_length=10)
@@ -21,7 +21,40 @@ class Usuario(models.Model):
     contrasenia = models.CharField(max_length=40)
     estado = models.CharField(max_length=40)
 
-class Prueba2 (models.Model):
-    holi = models.CharField(max_length=40)
-    prueba = models.CharField(max_length=40)
-    kk = models.CharField(max_length=40)
+#class Admin(models.Model):
+
+class Veterinary(models.Model):
+    fecha_graduacion = models.DateField()
+    universidad = models.CharField(max_length= 60)
+    fecha_registro = models.DateField()
+    calificacion = models.FloatField()
+    numero_tarjeta_profesional = models.IntegerField()
+
+class Speciality(models.Model):
+    nombre = models.CharField(max_length= 60)
+    descripcion =  models.CharField(max_length= 90)
+    veterinaries = models.ManyToManyField(Veterinary,blank=True)
+
+class Veterinary_files(models.Model):
+    nombre = models.CharField(max_length= 60)
+    extension = models.CharField(max_length= 60)
+    archivo = models.CharField(max_length= 1000)
+    veterinary = models.ForeignKey(Veterinary, on_delete=models.CASCADE)
+
+class Rol(models.Model):
+    nombre = models.CharField(max_length= 60)
+    descripcion = models.CharField(max_length=100)
+    user = models.ManyToManyField(User,blank=True)
+
+class Functionality(models.Model):
+    nombre = models.CharField(max_length = 60)
+    url = models.CharField(max_length = 200)
+    descripcion = models.CharField(max_length = 100)
+    rol = models.ManyToManyField(Rol,blank=True)
+
+
+
+
+
+
+
