001b5b8a8bf985c08fea508e48919107ddfa453c
dasilvaca <dasilvaca@unal.edu.co>
2022-04-24 11:49:39 -0500

    xd

--- FrontDoggieHommie/src/app/app.component.ts
+++ FrontDoggieHommie/src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'DoggieHommie';
+  
+  
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {}
+  
+  onSubmit():void{
+    console.log("Submitted");
+  }  
+  
 }
