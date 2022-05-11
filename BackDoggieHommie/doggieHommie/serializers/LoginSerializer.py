3060e2a1586419d24d9662a4d81b196698a2735b
lceballosa1 <55591683+lauraaac@users.noreply.github.com>
2022-05-11 14:46:04 -0500

    ajustes

--- BackDoggieHommie/doggieHommie/serializers/LoginSerializer.py
+++ BackDoggieHommie/doggieHommie/serializers/LoginSerializer.py
@@ -1,12 +1,13 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.contrib.auth.models import update_last_login 
-
+from doggieHommie.models import User
 
 class LoginSerializer (TokenObtainPairSerializer):
     
     def validate(self, attrs):
         data = super().validate(attrs)
-        data["idUser"] = self.user.id
+        userApp = User.objects.get(user=self.user)
+        data["idUser"] = userApp.id
         data["nombreUser"] = self.user.first_name
         data["lastLogin"] = self.user.last_login
         update_last_login(None, self.user)
