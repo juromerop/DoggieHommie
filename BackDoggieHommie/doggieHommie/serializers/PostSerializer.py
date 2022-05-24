fadf28f0827ba842c1365919a0dc11e664d195e7
lauraaac <lceballosa@unal.edu.co>
2022-05-23 19:57:13 -0500

    add paginación

--- BackDoggieHommie/doggieHommie/serializers/PostSerializer.py
+++ BackDoggieHommie/doggieHommie/serializers/PostSerializer.py
@@ -7,20 +7,20 @@ from .CommentSerializer import CommentSerializer
 
 class PostSerializer (serializers.ModelSerializer):
     bankAccounts = BankAccountSerializer(many=True, read_only=True)
-    comments = CommentSerializer(many=True, read_only=True)
+    #comments = CommentSerializer(many=True, read_only=True)
     userData = UserSerializer(read_only=True, source='user')
     user = serializers.PrimaryKeyRelatedField( write_only=True, queryset = User.objects.all())
     
     
     idBankAccount = serializers.PrimaryKeyRelatedField( write_only=True, queryset =BankAccounts.objects.all(),
                                                        source='bankAccounts', many = True )
-    idComment= serializers.PrimaryKeyRelatedField( write_only=True, queryset =Comment.objects.all(),
-                                                       source='comments', many = True )
+   # idComment= serializers.PrimaryKeyRelatedField( write_only=True, queryset =Comment.objects.all(),
+    #                                                   source='comments', many = True )
         
     class Meta:
         model = Post
         fields =  ['id','title', 'description', 'date', 'grade', 'isDonation', 'state', 'number_banned', 
-                   'user', 'bankAccounts', 'idBankAccount', 'userData', 'comments', 'idComment']
+                   'user', 'bankAccounts', 'idBankAccount', 'userData']# 'comments', 'idComment']
     
     
         
